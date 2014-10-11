4bef319a2be69109feb4d32ed7323744174d8b30
Stormbringer <stormbringer-s@mail.ru>
2014-10-11 20:09:06 +0400

    add server icon

--- MongoMS/ViewModel/DatabaseExplorerDBViewModel.cs
+++ MongoMS/ViewModel/DatabaseExplorerDBViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Driver;
 
 namespace MongoMS.ViewModel
 {
@@ -15,7 +17,14 @@ namespace MongoMS.ViewModel
         {
             _cs = cs;
             _db = db;
+
+            var dbo = new MongoClient(cs).GetServer().GetDatabase(db).GetCollectionNames();
+
+
+
+            Collections = new ObservableCollection<string>(dbo);
         }
         public string Name { get { return _db; } }
+        public ObservableCollection<string> Collections { get; private set; } 
     }
 }
