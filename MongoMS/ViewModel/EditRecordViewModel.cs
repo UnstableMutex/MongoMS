496db85491640b83bca99d1f37f2cfef064007d6
Stormbringer <stormbringer-s@mail.ru>
2014-10-20 20:49:01 +0400

    find criteria worked

--- MongoMS/ViewModel/EditRecordViewModel.cs
+++ MongoMS/ViewModel/EditRecordViewModel.cs
@@ -1,10 +1,12 @@
+using System.Windows.Input;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using MVVMLight.Extras;
 
 namespace MongoMS.ViewModel
 {
     [Header("Правка записи")]
-    internal class EditRecordViewModel
+    internal class EditRecordViewModel:VMB
     {
         private readonly MongoCollection<BsonDocument> _coll;
         private readonly BsonDocument _selected;
@@ -13,6 +15,13 @@ namespace MongoMS.ViewModel
         {
             _coll = coll;
             _selected = selected;
+            AssignCommands<NoWeakRelayCommand>();
         }
+        public string DocumentString { get; set; }
+        public string FieldName { get; set; }
+        public string Value { get; set; }
+        public ICommand GenerateGuidCommand { get; private set; }
+        void GenerateGuid()
+        { }
     }
 }
--- MongoMS/ViewModel/FindViewModel.cs
+++ MongoMS/ViewModel/FindViewModel.cs
@@ -9,56 +9,54 @@ using GalaSoft.MvvmLight.Ioc;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using MVVMLight.Extras;
-
 namespace MongoMS.ViewModel
 {
-   [Header("Поиск")]
-    class FindViewModel:VMB
+    [Header("Поиск")]
+    class FindViewModel : VMB
     {
-       private readonly MongoCollection<BsonDocument> _coll;
-       //private readonly string _cs;
-        //private readonly string _db;
-        //private readonly string _coll;
-       private IEnumerable<BsonDocument> _queryResults;
-       private BsonDocument _selected;
-
-       public FindViewModel(MongoCollection<BsonDocument> coll)
+        private readonly MongoCollection<BsonDocument> _coll;
+        private IEnumerable<BsonDocument> _queryResults;
+        private BsonDocument _selected;
+        private string _findCriteria;
+        public FindViewModel(MongoCollection<BsonDocument> coll)
         {
-           _coll = coll;
-         
-          
+            _coll = coll;
             QueryResults = _coll.FindAll().SetLimit(100).ToList();
-          
-           AssignCommands<NoWeakRelayCommand>();
+            AssignCommands<NoWeakRelayCommand>();
         }
-       public ICommand EditCommand { get; private set; }
-
-       void Edit()
-       {
-           SimpleIoc.Default.GetInstance<MainViewModel>().Content.Add(new EditRecordViewModel(_coll, Selected));
-           
-       }
-
-       public BsonDocument Selected
-       {
-           get { return _selected; }
-           set
-           {
-               _selected = value;
-               RaisePropertyChangedNoSave();
-           }
-       }
-
-       public IEnumerable<BsonDocument> QueryResults
-       {
-           get { return _queryResults; }
-           set
-           {
-               _queryResults = value;
-               RaisePropertyChangedNoSave();
-           }
-       }
-
-       public DataTable dt { get; set; }
+        public ICommand EditCommand { get; private set; }
+        void Edit()
+        {
+            SimpleIoc.Default.GetInstance<MainViewModel>().Content.Add(new EditRecordViewModel(_coll, Selected));
+        }
+        public ICommand FindCommand { get; private set; }
+        void Find()
+        {
+            var doc = new QueryDocument(BsonDocument.Parse(FindCriteria));
+            QueryResults = _coll.Find(doc);
+        }
+        public string FindCriteria
+        {
+            get { return _findCriteria; }
+            set { _findCriteria = value; }
+        }
+        public BsonDocument Selected
+        {
+            get { return _selected; }
+            set
+            {
+                _selected = value;
+                RaisePropertyChangedNoSave();
+            }
+        }
+        public IEnumerable<BsonDocument> QueryResults
+        {
+            get { return _queryResults; }
+            set
+            {
+                _queryResults = value;
+                RaisePropertyChangedNoSave();
+            }
+        }
     }
 }
