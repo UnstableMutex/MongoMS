5651c055d56ef5f1fd1fc930600c30886bb8e315
Stormbringer <stormbringer-s@mail.ru>
2014-10-22 11:18:57 +0400

    add doc add field window features added

--- MongoMS/ViewModel/AddDocumentViewModel.cs
+++ MongoMS/ViewModel/AddDocumentViewModel.cs
@@ -17,11 +17,12 @@ namespace MongoMS.ViewModel
         private readonly MongoCollection<BsonDocument> _coll;
 
         private string _document;
+        private BsonDocument _doc;
 
         public AddDocumentViewModel(MongoCollection<BsonDocument> coll)
         {
             _coll = coll;
-
+            Doc=new BsonDocument();
             AssignCommands<NoWeakRelayCommand>();
         }
 
@@ -29,42 +30,62 @@ namespace MongoMS.ViewModel
         [BsonDocumentValidator("sfdsfsdf")]
         public string Document
         {
-            get { return _document; }
+            get { return Doc.ToString(); }
+          
+        }
+
+        private BsonDocument Doc
+        {
+            get { return _doc; }
             set
             {
-                _document = value;
-                try
-                {
-                    RaisePropertyChanged();
-                }
-                catch (Exception)
-                {
-
-                    throw;
-                }
-
+                _doc = value;
+               RaisePropertyChangedNoSave("Document");
             }
+        }
+
+        public string FieldName { get; set; }
+        public string FieldValue { get; set; }
+        public ICommand AddFieldCommand { get; private set; }
+
+        void AddField()
+        {
+            AddField(new BsonElement(FieldName,FieldValue));
         }
         public ICommand AddGuidIdCommand { get; private set; }
 
+
+        void AddField(BsonElement element)
+        {
+            Doc.Add(element);
+            RaisePropertyChangedNoSave("Document");
+        }
+
+
         void AddGuidId()
         {
-            BsonDocument d=new BsonDocument();
-            if (!string.IsNullOrEmpty(Document))
-            {
-                d= BsonDocument.Parse(Document);
-            }
+
+            AddField(new BsonElement("_id",Guid.NewGuid()));
+
+
+
+
+            //BsonDocument d=new BsonDocument();
+            //if (!string.IsNullOrEmpty(Document))
+            //{
+            //    d= BsonDocument.Parse(Document);
+            //}
            
-            d["_id"] = Guid.NewGuid();
-            Document = d.ToString();
+            //d["_id"] = Guid.NewGuid();
+            //Document = d.ToString();
         }
         public ICommand AddDocumentCommand { get; private set; }
 
         void AddDocument()
         {
           
-            _coll.Insert(BsonDocument.Parse(Document));
-            Document = string.Empty;
+            _coll.Insert(Doc);
+           Doc=new BsonDocument();
         }
 
     }
