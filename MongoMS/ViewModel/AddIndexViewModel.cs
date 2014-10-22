651b040c5bfc28f347302cb162a1c3ab31390530
Stormbringer <stormbringer-s@mail.ru>
2014-10-22 11:27:17 +0400

    added some windows

---
+++ MongoMS/ViewModel/AddIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMLight.Extras;
+
+namespace MongoMS.ViewModel
+{
+    class AddIndexViewModel:VMB
+    {
+    }
+}
