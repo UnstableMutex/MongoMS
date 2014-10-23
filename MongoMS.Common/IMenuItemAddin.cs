43a6ee903a144d07cdfc461d48aa884e92f5849a
Stormbringer <stormbringer-s@mail.ru>
2014-10-23 15:42:09 +0400

    added common assembly

---
+++ MongoMS.Common/IMenuItemAddin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoMS.Common
+{
+    interface IMenuItemAddin
+    {
+        Type View { get; }
+        Type ViewModel { get; }
+    }
+}
