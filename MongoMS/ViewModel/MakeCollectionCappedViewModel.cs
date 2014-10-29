e2becc5e6f48694f9b97f49fc6e852b13cdc59bb
Stormbringer <stormbringer-s@mail.ru>
2014-10-29 20:12:22 +0300

    make capped interface dev

--- MongoMS/ViewModel/MakeCollectionCappedViewModel.cs
+++ MongoMS/ViewModel/MakeCollectionCappedViewModel.cs
@@ -3,11 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
 using MVVMLight.Extras;
 
 namespace MongoMS.ViewModel
 {
     class MakeCollectionCappedViewModel:VMB
     {
+        private readonly MongoCollection _coll;
+
+        public MakeCollectionCappedViewModel(MongoCollection coll)
+        {
+            _coll = coll;
+        }
+
+        public ICommand OKCommand { get; private set; }
+
+        private void OK()
+        {
+            var g = Guid.NewGuid();
+          var s=  g.ToString();
+            CollectionOptionsBuilder b=new CollectionOptionsBuilder();
+            b.SetCapped(true).SetMaxDocuments(MaxCount).SetMaxSize(MaxSize);
+          _coll.Database.CreateCollection(s, b);
+            
+            var newcoll = _coll.Database.GetCollection(s);
+            foreach (var item in _coll.FindAllAs<BsonDocument>())
+            {
+                newcoll.Save(item);
+            }
+            _coll.Drop();
+        }
+
+        public int MaxSize { get; set; }
+        public int MaxCount { get; set; }
+
     }
 }
