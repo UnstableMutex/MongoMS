b06610206a2aae8efa400b23846da929f9f9f0c8
Stormbringer <stormbringer-s@mail.ru>
2014-10-30 18:04:26 +0400

    addindex designing

--- MongoMS/ViewModel/AddIndexViewModel.cs
+++ MongoMS/ViewModel/AddIndexViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Input;
 using MongoDB.Bson;
@@ -8,7 +9,7 @@ using MVVMLight.Extras;
 
 namespace MongoMS.ViewModel
 {
-    class IndexInfo:IEquatable<IndexInfo>
+    class IndexInfo : IEquatable<IndexInfo>
     {
 
         public string Name { get; set; }
@@ -19,9 +20,14 @@ namespace MongoMS.ViewModel
             return Field == other.Field && Order == other.Order;
         }
 
+        public override int GetHashCode()
+        {
+            return Field.GetHashCode() * Order.GetHashCode();
+        }
+
         public override bool Equals(object obj)
         {
-            return Equals((IndexInfo) obj);
+            return Equals((IndexInfo)obj);
         }
 
     }
@@ -30,15 +36,34 @@ namespace MongoMS.ViewModel
     {
         private readonly MongoCollection<BsonDocument> _coll;
         private IEnumerable<string> _fieldNames;
+        private string _selectedField;
+        private string _indexName;
+        private sbyte _direction;
 
         public AddIndexViewModel(MongoCollection<BsonDocument> coll)
         {
             _coll = coll;
-            var fns = GetFieldNames();
-            var ind = coll.GetIndexes();
-
-            Indexes = new PairedObservableCollections<string>(fns);
+            FieldNames = GetFieldNames();
+            ExistsIndexes = new ObservableCollection<IndexInfo>(GetExistsIndexes());
+            Direction = 1;
+            AssignCommands<NoWeakRelayCommand>();
         }
+
+        private IEnumerable<IndexInfo> GetExistsIndexes()
+        {
+            var res = _coll.GetIndexes();
+            foreach (var ind in res)
+            {
+                var ii = new IndexInfo();
+                var key = ind.Key.Single();
+                ii.Name = ind.Name;
+                ii.Field = key.Name;
+                ii.Order = sbyte.Parse(key.Value.ToString());
+                yield return ii;
+            }
+        }
+        public ObservableCollection<IndexInfo> ExistsIndexes { get; private set; }
+
 
         public ICommand OKCommand { get; private set; }
 
@@ -46,7 +71,7 @@ namespace MongoMS.ViewModel
         {
 
         }
-        public PairedObservableCollections<string> Indexes { get; private set; }
+
         private IEnumerable<string> GetFieldNames()
         {
             var cur = _coll.FindAll().SetLimit(100);
@@ -57,7 +82,51 @@ namespace MongoMS.ViewModel
             }
             return fields;
         }
+        public IEnumerable<string> FieldNames { get; private set; }
 
+        public string IndexName
+        {
+            get { return _indexName; }
+            set
+            {
+                _indexName = value;
+                RaisePropertyChangedNoSave();
+            }
+        }
 
+        public sbyte Direction
+        {
+            get { return _direction; }
+            set
+            {
+                _direction = value; 
+                RaisePropertyChangedNoSave();
+                ChangeIndexName();
+            }
+        }
+
+        public ICommand ChangeDirectionCommand { get; private set; }
+
+        private void ChangeDirection()
+        {
+            Direction *= -1;
+        }
+
+        public string SelectedField
+        {
+            get { return _selectedField; }
+            set
+            {
+                _selectedField = value; 
+                RaisePropertyChangedNoSave();
+                ChangeIndexName();
+              
+            }
+        }
+
+        private void ChangeIndexName()
+        {
+            IndexName = SelectedField + (Direction == 1 ? "Asc" : "Desc") + "Index";
+        }
     }
 }
