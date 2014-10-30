052c5ef934b73d2a559427115565b4efbda95416
Stormbringer <stormbringer-s@mail.ru>
2014-10-30 22:24:19 +0400

    mainwindow save position now

--- MongoMS/MainWindow.xaml.cs
+++ MongoMS/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,21 @@ namespace MongoMS
         public MainWindow()
         {
             InitializeComponent();
+            Width = WindowPosition.Default.Width;
+            Left = WindowPosition.Default.Left;
+            Height = WindowPosition.Default.Height;
+            Top = WindowPosition.Default.Top;
+
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            WindowPosition.Default.Width = (int)Width;
+            WindowPosition.Default.Left = (int)Left;
+            WindowPosition.Default.Height = (int)Height;
+            WindowPosition.Default.Top = (int)Top;
+            WindowPosition.Default.Save();
         }
     }
 }
