d5512e7e0f3da6ba6a0cbc0d73cd11bda1f2be70
Stormbringer <stormbringer-s@mail.ru>
2014-10-30 11:26:56 +0400

    rename draft writing

--- MongoMS/ViewModel/RenameFieldsViewModel.cs
+++ MongoMS/ViewModel/RenameFieldsViewModel.cs
@@ -4,23 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using MongoDB.Bson;
+using MongoDB.Driver;
 using MVVMLight.Extras;
 
 namespace MongoMS.ViewModel
 {
-    class RenameFieldsViewModel:VMB
+    class RenameFieldsViewModel : VMB
     {
-        public RenameFieldsViewModel()
+        private readonly MongoCollection _coll;
+
+        public RenameFieldsViewModel(MongoCollection coll)
         {
+            _coll = coll;
             AssignCommands<NoWeakRelayCommand>();
         }
+
         public string OldName { get; set; }
         public string NewName { get; set; }
         public ICommand OKCommand { get; private set; }
 
         private void OK()
         {
-            
+       //TODO test rename
+            UpdateDocument ud = new UpdateDocument();
+            ud.Add("$rename", new BsonDocument(OldName, NewName));
+            _coll.Update(new QueryDocument(), ud);
         }
     }
 }
