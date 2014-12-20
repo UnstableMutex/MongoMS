d398b1592e777ea128b66debdb1e52bdce3f77fc
Stormbringer <stormbringer-s@mail.ru>
2014-12-20 22:25:48 +0400

    need text input control on findwindow

--- MongoMS/View/FindView.xaml.cs
+++ MongoMS/View/FindView.xaml.cs
@@ -23,7 +23,7 @@ namespace MongoMS.View
         {
             try
             {
-                var dc = (FindViewModel) ((sender as DataGrid).DataContext);
+                var dc = (FindViewModel)((sender as DataGrid).DataContext);
                 dc.Selected = e.AddedItems[0] as BsonDocument;
             }
             catch (IndexOutOfRangeException exception)
@@ -34,36 +34,58 @@ namespace MongoMS.View
 
         private void FindView_OnLoaded(object sender, RoutedEventArgs e)
         {
-              FocusManager.SetFocusedElement(this, FindTextBox);
+            FocusManager.SetFocusedElement(this, FindTextBox);
             Keyboard.Focus(FindTextBox);
         }
 
         private void FindTextBox_OnGotFocus(object sender, RoutedEventArgs e)
         {
-            if (FindTextBox.Text=="{}")
-            FindTextBox.SelectionStart = 1;
+            if (FindTextBox.Text == "{}")
+                FindTextBox.SelectionStart = 1;
         }
-        private void FindTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+
+
+        private void FindTextBox_OnPreviewKeyDown(object sender, KeyEventArgs e)
         {
-            var selstart = FindTextBox.SelectionStart;
-            var text = FindTextBox.Text;
-            var firstchange = e.Changes.First();
-            var start = text.Substring(0, firstchange.Offset);
-            var end = text.Substring(firstchange.Offset + 1);
 
-            var addedsymb = text.Substring(firstchange.Offset, 1);
-            switch (addedsymb)
-            {
-                case "'":
-                   FindTextBox.  Text = start + "''" + end;
-                     FindTextBox.SelectionStart = selstart;
-                    break;
-                default:
-                    return;
-            }
-        
+          FindCriteriaInputController.OverrideInput(FindTextBox,e);
         }
 
-       
+        private void FindTextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            /*
+            http://professorweb.ru/my/WPF/base_WPF/level5/5_8.php
+             textcompositionmanager
+             почитать
+            
+             Наиболее подходящим вариантом является обработка события PreviewTextlnput (где выполняется большая часть проверки) 
+             в сочетании с событием PreviewKeyDown для нажатий тех клавиш, которые не генерируют событие PreviewTextInput в текстовом поле (например, пробела).
+            
+             */
+        }
     }
+
+   static class FindCriteriaInputController
+    {
+       public static void OverrideInput(TextBox tb, KeyEventArgs eventArgs)
+       {
+           if (eventArgs.Key == Key.Oem7&&tb.SelectionLength==0) // ' key
+           {
+               WorkOEM7(tb, eventArgs);
+               return;
+           }
+       }
+
+       private static void WorkOEM7(TextBox tb, KeyEventArgs eventArgs)
+       {
+           //var text = tb.Text;
+           //var start = text.Substring(0, tb.SelectionStart);
+           //var end = text.Substring(tb.SelectionStart);
+           //var newText = start + "''" + end;
+           //tb.Text = newText;
+           //tb.SelectionStart = tb.SelectionStart + 1;
+           //eventArgs.Handled = true;
+       }
+    }
+
 }
