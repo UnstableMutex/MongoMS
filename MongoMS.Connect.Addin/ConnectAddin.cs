6b9850a6696b07502f3c0df4d07ceaf26151dc93
Stormbringer <stormbringer-s@mail.ru>
2015-01-07 18:42:21 +0400

    delete blanks

--- MongoMS.Connect.Addin/ConnectAddin.cs
+++ MongoMS.Connect.Addin/ConnectAddin.cs
@@ -10,20 +10,17 @@ using Microsoft.Practices.Prism.Regions;
 using Microsoft.Practices.Unity;
 using MongoMS.Common;
 using MongoMS.Connect.Addin.View;
-
 namespace MongoMS.Connect.Addin
 {
     public class ConnectAddin : IModule
     {
         private readonly IRegionManager _regionManager;
         private readonly IUnityContainer _unity;
-
         public ConnectAddin(IRegionManager regionManager, IUnityContainer unity)
         {
             _regionManager = regionManager;
             _unity = unity;
         }
-
         Button GetConnectButton()
         {
             Button b = new Button();
@@ -31,28 +28,22 @@ namespace MongoMS.Connect.Addin
             b.Click += b_Click;
             return b;
         }
-
         void b_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             _regionManager.AddToRegion(RegionNames.TabControlRegion, GetConnectTab());
-
         }
-
         private UserControl GetConnectTab()
         {
             return _unity.Resolve<MainView>();
         }
-
         void close_Click(object sender, RoutedEventArgs e)
         {
             var b = (sender as Button);
             _regionManager.Regions[RegionNames.TabControlRegion].Remove(b.Tag);
         }
-
         public void Initialize()
         {
             _regionManager.AddToRegion(RegionNames.TopMenuRegion, GetConnectButton());
         }
     }
-
 }
--- MongoMS.Connect.Addin/ViewModel/MainViewModel.cs
+++ MongoMS.Connect.Addin/ViewModel/MainViewModel.cs
@@ -14,14 +14,12 @@ using Microsoft.Practices.Prism.PubSubEvents;
 using Microsoft.Practices.Unity;
 using MongoDB.Driver;
 using MongoMS.Common;
-
 namespace MongoMS.Connect.Addin.ViewModel
 {
     public class MainViewModel : OKViewModel
     {
         private readonly IUnityContainer _unity;
         private readonly IEventAggregator _eventAggregator;
-
         public MainViewModel(IUnityContainer unity, IEventAggregator eventAggregator)
         {
             _unity = unity;
@@ -30,43 +28,34 @@ namespace MongoMS.Connect.Addin.ViewModel
             AddNewCommand = new DelegateCommand(AddNew);
         }
         private ObservableCollection<ConnectionViewModel> _connections;
-
         public ObservableCollection<ConnectionViewModel> Connections
         {
             get { return _connections; }
             private set { _connections = value; }
         }
         private ConnectionViewModel _selected;
-
         public ConnectionViewModel Selected
         {
             get { return _selected; }
             set { SetProperty(ref _selected, value); }
         }
         public ICommand AddNewCommand { get; private set; }
-
         private void AddNew()
         {
-
-
             var conn = _unity.Resolve<ConnectionViewModel>();
             conn.Name = "local";
             conn.CS.Server = new MongoServerAddress("localhost");
             Connections.Add(conn);
             Selected = conn;
-
         }
-
         public override string Header
         {
             get { return "Connect"; }
         }
-
         public override void BeforeClose()
         {
             MessageBox.Show("dsfsdf");
         }
-
         protected override void OK()
         {
             var e = _eventAggregator.GetEvent<PubSubEvent<RequestConnect>>();
