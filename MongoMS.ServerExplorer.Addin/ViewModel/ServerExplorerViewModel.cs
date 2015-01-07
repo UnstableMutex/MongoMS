5ca6b88cc6861d31ed22e0a084f9fc765f9fad9a
Stormbringer <stormbringer-s@mail.ru>
2015-01-07 22:55:02 +0400

    treeview level2

--- MongoMS.ServerExplorer.Addin/ViewModel/ServerExplorerViewModel.cs
+++ MongoMS.ServerExplorer.Addin/ViewModel/ServerExplorerViewModel.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Practices.Prism.Mvvm;
 using Microsoft.Practices.Prism.PubSubEvents;
+using MongoDB.Driver;
 
 namespace MongoMS.ServerExplorer.Addin.ViewModel
 {
@@ -30,11 +31,40 @@ namespace MongoMS.ServerExplorer.Addin.ViewModel
         {
             _name = name;
             _cs = cs;
+            InitChildren();
         }
+
+        private void InitChildren()
+        {
+                        Children = new ObservableCollection<DatabaseViewModel>();
+            MongoServer s = new MongoClient(_cs).GetServer();
+            foreach (var dbname in s.GetDatabaseNames())
+            {
+                Children.Add( new DatabaseViewModel(s.GetDatabase(dbname)));
+            }
+
+        }
+
+        public ObservableCollection<DatabaseViewModel> Children { get; private set; } 
 
         public string Name
         {
             get { return _name; }
+        }
+    }
+    public class DatabaseViewModel
+    {
+        private readonly MongoDatabase _db;
+
+
+        public DatabaseViewModel(MongoDatabase db)
+        {
+            _db = db;
+        }
+
+        public string Name
+        {
+            get { return _db.Name; }
         }
     }
 }
