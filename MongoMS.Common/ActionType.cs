ecd694783ca99a4642863eb7857f34f569bd00d3
Stormbringer <stormbringer-s@mail.ru>
2015-01-08 12:45:53 +0400

    newfiles

---
+++ MongoMS.Common/ActionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Effects;
+
+namespace MongoMS.Common
+{
+   public enum ActionType
+    {
+        Drop,Create,Rename
+    }
+}
---
+++ MongoMS.Common/Events/DatabaseAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace MongoMS.Common.Events
+{
+   public class DatabaseAction
+   {
+       public MongoServer Server { get; set; }
+       public string DatabaseName { get; set; }
+       public ActionType Action { get; set; }
+   }
+}
