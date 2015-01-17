48105dbd3b05080f6c4a9060b24fa1ba9a28984d
Stormbringer <stormbringer-s@mail.ru>
2015-01-17 23:03:31 +0400

    export to mssql addin draft added

---
+++ MongoMS.ExportToMSSQL.Addin/View/MainView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MongoMS.ExportToMSSQL.Addin.ViewModel;
+
+namespace MongoMS.ExportToMSSQL.Addin.View
+{
+    /// <summary>
+    /// Логика взаимодействия для MainView.xaml
+    /// </summary>
+    public partial class MainView : UserControl
+    {
+        public MainView(MainViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+}
