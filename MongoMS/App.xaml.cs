b750727703492c23b1c945625935e00e4066df5c
Stormbringer <stormbringer-s@mail.ru>
2015-02-07 13:39:29 +0400

    conflicts resolved

--- MongoMS/App.xaml.cs
+++ MongoMS/App.xaml.cs
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-﻿using System.Reflection;
-using System.Windows;
-using GalaSoft.MvvmLight.Ioc;
-using MongoMS.ViewModel;
-using MVVMTemplateSelection;
-
-namespace MongoMS
-{
-    /// <summary>
-    ///     Логика взаимодействия для App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-        private MainViewModel mwm;
-
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            TypeSource.AddAssembly(Assembly.GetExecutingAssembly());
-            base.OnStartup(e);
-            SimpleIoc.Default.Register<MainViewModel>();
-            mwm = SimpleIoc.Default.GetInstance<MainViewModel>();
-            var mw = new MainWindow();
-            mw.DataContext = mwm;
-            mw.Show();
-        }
-
-        protected override void OnExit(ExitEventArgs e)
-        {
-            base.OnExit(e);
-            mwm.Save();
-        }
-    }
-}
-=======
 ﻿using System.Windows;
 using Microsoft.Practices.Prism;
 
@@ -51,4 +16,3 @@ namespace MongoMS
         }
     }
 }
->>>>>>> origin/prismver
