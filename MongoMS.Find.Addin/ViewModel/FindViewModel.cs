4bfada9a96aef171be007c8d4fffb59b8d74276a
Stormbringer <stormbringer-s@mail.ru>
2015-02-08 14:47:19 +0400

    find coding

--- MongoMS.Find.Addin/ViewModel/FindViewModel.cs
+++ MongoMS.Find.Addin/ViewModel/FindViewModel.cs
@@ -1,11 +1,15 @@
-﻿using MongoDB.Driver;
+﻿using System.Collections;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Driver;
 using MongoMS.Common;
 
 namespace MongoMS.Find.Addin.ViewModel
 {
     public class FindViewModel:CollectionOKViewModel
     {
-       
+        private IEnumerable<BsonDocument> _queryResults;
+
         public FindViewModel(MongoCollection collection):base(collection)
         {
             
@@ -16,5 +20,18 @@ namespace MongoMS.Find.Addin.ViewModel
             get { return "Find"; }
         }
 
+        protected override void OK()
+        {
+            QueryResults = Collection.FindAllAs<BsonDocument>();
+        }
+
+        public IEnumerable<BsonDocument> QueryResults
+        {
+            get { return _queryResults; }
+            set
+            {
+                this.SetProperty(ref _queryResults, value);
+            }
+        }
     }
 }
