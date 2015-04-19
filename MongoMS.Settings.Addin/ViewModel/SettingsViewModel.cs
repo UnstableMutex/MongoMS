fef734e0d1b36df32106f677b830afc815e3b0f9
Stormbringer <stormbringer-s@mail.ru>
2015-04-19 14:30:16 +0400

    added settings addin

---
+++ MongoMS.Settings.Addin/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Mvvm;
+using MongoMS.Common;
+
+namespace MongoMS.Settings.Addin.ViewModel
+{
+    class SettingsViewModel:BindableBase,ITabContent
+    {
+        public string Header { get { return "Settings"; } }
+    }
+}
